8e3374e7679d63925a2e6bb158cfd669a7c9873b
GTech1256 <pomah156@gmail.com>
2019-10-07 14:10:24 +0300

    add travis

--- src/server/config/mongoose.js
+++ src/server/config/mongoose.js
@@ -14,41 +14,41 @@ mongoose.connection.on('error', (err) => {
 // print mongoose logs in dev env
 if (process.env.NODE_ENV === 'development') {
   mongoose.set('debug', true);
-}
-/* set migrations */
-mongoose.connection.on('open', async () => {
-  const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed semper sem. Integer tempor condimentum varius. Ut ut eros tincidunt, imperdiet nunc id, aliquet magna. Donec dignissim tortor erat, id semper sem feugiat vel. Sed dictum mattis accumsan. Aliquam commodo dolor augue. Sed tincidunt leo non lacinia mattis. Maecenas dapibus, odio sed finibus bibendum, nibh purus tempus nisl, sollicitudin tempor enim dui id libero. Suspendisse semper, felis eu vehicula commodo, justo diam laoreet ipsum, quis dictum ex risus luctus dui. Pellentesque sit amet commodo ligula. Aenean quis elementum massa. Suspendisse pulvinar justo eu ligula iaculis porta. Pellentesque ut elit iaculis diam suscipit ultricies.';
-  const migrationsNames = [
-    lorem.substring(0, 10),
-    lorem.substring(10, 20),
-    lorem.substring(20, 30),
-    lorem.substring(30, 40),
-    lorem.substring(40, 50),
-    lorem.substring(50, 60),
-  ];
-  console.log('-=MIGRATIONS STARTED=-');
 
-  // eslint-disable-next-line no-restricted-syntax
-  for (const migrationName of migrationsNames) {
-    // eslint-disable-next-line no-await-in-loop
-    const migrationGood = await Good.findOne({ name: migrationName });
-    // console.log(migrationGood);
-    if (!migrationGood) {
+  /* set migrations */
+  mongoose.connection.on('open', async () => {
+    const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed semper sem. Integer tempor condimentum varius. Ut ut eros tincidunt, imperdiet nunc id, aliquet magna. Donec dignissim tortor erat, id semper sem feugiat vel. Sed dictum mattis accumsan. Aliquam commodo dolor augue. Sed tincidunt leo non lacinia mattis. Maecenas dapibus, odio sed finibus bibendum, nibh purus tempus nisl, sollicitudin tempor enim dui id libero. Suspendisse semper, felis eu vehicula commodo, justo diam laoreet ipsum, quis dictum ex risus luctus dui. Pellentesque sit amet commodo ligula. Aenean quis elementum massa. Suspendisse pulvinar justo eu ligula iaculis porta. Pellentesque ut elit iaculis diam suscipit ultricies.';
+    const migrationsNames = [
+      lorem.substring(0, 10),
+      lorem.substring(10, 20),
+      lorem.substring(20, 30),
+      lorem.substring(30, 40),
+      lorem.substring(40, 50),
+      lorem.substring(50, 60),
+    ];
+    console.log('-=MIGRATIONS STARTED=-');
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const migrationName of migrationsNames) {
       // eslint-disable-next-line no-await-in-loop
-      await Good.create({
-        name: migrationName,
-        quantity: Math.round(Math.random() * 999),
-        price: {
-          currency: Math.random() > 0.5 ? 'RUB' : 'USD',
-          value: Math.random() * 100,
-        },
-      });
+      const migrationGood = await Good.findOne({ name: migrationName });
+      // console.log(migrationGood);
+      if (!migrationGood) {
+        // eslint-disable-next-line no-await-in-loop
+        await Good.create({
+          name: migrationName,
+          quantity: Math.round(Math.random() * 999),
+          price: {
+            currency: Math.random() > 0.5 ? 'RUB' : 'USD',
+            value: Math.random() * 100,
+          },
+        });
+      }
     }
-  }
 
-  console.log('-=MIGRATIONS ENDED=-');
-});
-
+    console.log('-=MIGRATIONS ENDED=-');
+  });
+}
 
 function getUrlForMongooseConnection() {
   console.log(process.env.NODE_ENV);
